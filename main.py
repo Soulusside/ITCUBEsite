52921ce5489516280bee4223e7c50526853354e0
Soulus <Juluur93@gmail.com>
2024-06-06 10:08:56 +0900

    Новая версия сайта, обновленной страницей news

--- main.py
+++ main.py
@@ -18,25 +18,37 @@ def index():
 def news():
     access_token = 'ecaef858ecaef858ecaef858b3efb86890eecaeecaef85889dc84fdd39a62ad2612e4bf'
     group_id = '213320340'
-    count = 15  # Количество постов, которые вы хотите получить
+    count = 15
 
     url = f'https://api.vk.com/method/wall.get?owner_id=-{group_id}&count={count}&access_token={access_token}&v=5.131'
     response = requests.get(url)
     data = response.json()
 
+    if 'response' not in data:
+        return "Error fetching data from VK API", 500
+
     posts = data['response']['items']
     for post in posts:
         attachments = post.get('attachments', [])
+        photos = []
+        videos = []
 
         for attachment in attachments:
             if attachment['type'] == 'photo':
-                # Получаем URL изображения
                 photo_url = attachment['photo']['sizes'][-1]['url']
+                photos.append(photo_url)
+            elif attachment['type'] == 'video':
+                video_id = attachment['video']['id']
+                owner_id = attachment['video']['owner_id']
+                access_key = attachment['video'].get('access_key', '')
+                video_url = f'https://vk.com/video{owner_id}_{video_id}' + (f'_{access_key}' if access_key else '')
+                videos.append(video_url)
 
-                # Добавляем URL изображения к посту
-                post.setdefault('photos', []).append(photo_url)
+        post['photos'] = photos
+        post['videos'] = videos
 
     return render_template('news.html', posts=posts)
+
 
 @app.route('/timedata')
 def timedata():
